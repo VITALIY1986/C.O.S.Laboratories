6df9a517ae3fa3ab5187c5afe3900ad90376b2d3
VITALIY1986 <staslego1986@gmail.com>
2022-09-12 18:25:44 +0300

    asd

--- next.config.js
+++ next.config.js
@@ -33,12 +33,10 @@ module.exports =   {
 };
 
 
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+  module.exports = withPWA({
+    // next.js config
+  })
